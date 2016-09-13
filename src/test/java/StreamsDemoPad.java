5e401fe0488d1191049b2ebc228ec179eed6596b
Sunit Parekh <parekh.sunit@gmail.com>
2016-09-13 22:54:11 +0530

    demo pad updated with some more examples

--- src/test/java/StreamsDemoPad.java
+++ src/test/java/StreamsDemoPad.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,21 +19,40 @@ public class StreamsDemoPad {
                 this.name = name;
                 this.birthDate = birthDate;
             }
+
+            int age() {
+                return Period.between(birthDate, LocalDate.now()).getYears();
+            }
+
+            @Override
+            public String toString() {
+                return "Student{" + name + '}';
+            }
         }
 
         List<Student> students = Arrays.asList(
                 new Student("Sunit", LocalDate.of(1977,8,7)),
-                new Student("Manan", LocalDate.of(2008,5,13)),
-                new Student("Leap Year Person", LocalDate.of(2012,5,13))
+                new Student("Sarthak", LocalDate.of(2008,5,13)),
+                new Student("Manish", LocalDate.of(1981,3,29)),
+                new Student("Gaurav", LocalDate.of(2012,5,13))
         );
 
+        // get names of all students
         Stream<String> namesStream = students.stream().map(s -> s.name);
         List<String> names = namesStream.collect(Collectors.toList());
         System.out.println(names);
 
-//        List<Student> leapYearStudents = students.stream()
-//                  .filter(s -> s.birthDate.isLeapYear() )
-//                  .collect(Collectors.toList());
+        // get list of all students who are born in leap year
+        List<Student> leapYearStudents = students.stream()
+                  .filter(s -> s.birthDate.isLeapYear() )
+                  .collect(Collectors.toList());
+        System.out.println(leapYearStudents);
+
+        // get students who are adult
+        List<Student> adults = students.stream()
+                .filter(s -> s.age() > 18)
+                .collect(Collectors.toList());
+        System.out.println(adults);
 
 //        Files.lines(Paths.get("pom.xml")).forEach(System.out::println);
     }
